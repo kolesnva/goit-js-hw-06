74b59304b91d9d7402ff54aa7b2e15b1467ccf69
Vladyslav Kolesnychenko <kolesnychenko_v@icloud.com>
2022-05-20 13:41:46 +0300

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const newIngredientEl = ingredients.map(ingredient => {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.textContent = `${ingredient}`;
+  ingredientsItem.classList.add('item');
+
+  return ingredientsItem;
+})
+
+const ingredientList = document.querySelector('#ingredients');
+ingredientList.append(...newIngredientEl);
+console.log(ingredientList);
