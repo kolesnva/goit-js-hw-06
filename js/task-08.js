d5076b94dbe3b31cf187032d0fa425dd1619423c
Vladyslav Kolesnychenko <kolesnychenko_v@icloud.com>
2022-05-24 18:48:53 +0300

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onSubmitClick);
+
+function onSubmitClick(event) {
+  event.preventDefault();
+
+  const loginInfo = {};
+  const emailInput = loginForm.elements[0].value;
+  const passwordInput = loginForm.elements[1].value;
+  
+  if (emailInput === ''|| passwordInput === '') {
+    alert('Fill all required fields');
+  }
+
+  loginInfo.email = emailInput;
+  loginInfo.password = passwordInput;
+
+  console.log(loginInfo);
+
+  loginForm.reset();
+}
