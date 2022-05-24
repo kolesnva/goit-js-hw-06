095d00478aa145aba37e8eae42f8ae74091d9632
Vladyslav Kolesnychenko <kolesnychenko_v@icloud.com>
2022-05-24 18:58:29 +0300

    working on task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const changeColorBtn = document.querySelector('.change-color');
+const bodyColor = document.querySelector('.color'); 
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick(event) {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
 }
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
