0fb7b34d4e9db239c64ca6ca3403978cd811eb2b
Vladyslav Kolesnychenko <kolesnychenko_v@icloud.com>
2022-05-25 08:52:48 +0300

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,16 @@
 const changeColorBtn = document.querySelector('.change-color');
-const bodyColor = document.querySelector('.color'); 
+const bodyColorSpan = document.querySelector('.color');
+let colorSpan = document.querySelector('span');
 
 changeColorBtn.addEventListener('click', onChangeColorBtnClick);
 
 function onChangeColorBtnClick(event) {
   document.body.style.backgroundColor = `${getRandomHexColor()}`;
-}
-
+  colorSpan.textContent = getRandomHexColor();
+};
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 };
